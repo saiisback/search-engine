57afe71fdf5bb06c420241e642f3c9fdad821a50
“saiisback” <karthiksaiketha@gmail.com>
2025-04-30 13:27:40 +0530

    feat: update metadata for the application

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Azizah',
+  description: 'made by sai karthik',
+  generator: 'sai karthik ketha',
 }
 
 export default function RootLayout({
